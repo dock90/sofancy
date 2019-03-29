0d74099f9920ec0e39ace191f3b17dd0457a185d
EdwardGoomba <ask@edwardvdesign.com>
2019-03-28 23:01:32 -0400

    Setup document page and main page

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Index = () => (
+  <div>
+    <Head>
+      <title>Index Page</title>
+      <meta
+        name="description"
+        content="This is the description of the Index page"
+      />
+    </Head>
+    <p>Content on Index Page</p>
+  </div>
+);
+
+export default Index;
