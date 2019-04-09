71e90a73c81e101d56861d417e669efa2db66617
EdwardGoomba <ask@edwardvdesign.com>
2019-04-08 22:30:54 -0400

    Basic sanity setup

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Head from 'next/head';
+import Header from '../components/Header';
 
 const Index = () => (
-  <div>
+  <div style={{ padding: '10px 45px' }}>
     <Head>
-      <title>Index Page</title>
+      <title>dock90</title>
       <meta
         name="description"
         content="This is the description of the Index page"
       />
     </Head>
-    <p>Content on Index Page</p>
+    <Header />
+    <h1>Welcome to dock90</h1>
+    <h2>Strategy, design, development.</h2>
+    <p>hello@dock90.io</p>
   </div>
 );
 
