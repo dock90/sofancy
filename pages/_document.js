3d5036e9a01fe8bab8fff8fe46de1211cc7113ca
EdwardGoomba <edward@dock90.io>
2019-12-07 13:32:22 -0500

    Add meta

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ class MyDocument extends Document {
       <html lang="en">
         <Head>
           <meta charSet="utf-8" />
+          <meta name="description" content="I help fast growing companies scale up to meet exponentially increasing customer demands." />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
