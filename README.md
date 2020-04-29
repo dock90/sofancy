e9f55abfd771fcf54afdfd5e63d42802fed4ea8b
EdwardGoomba <edward@dock90.io>
2020-04-29 10:38:39 -0400

    Reset to base

--- README.md
+++
@@ -1,3 +0,0 @@
-# sofancy
-
-Dock90 front end. Built with next, ssr material styles, and sanity to run the blog.
--- components/styled/SharedStyles.js
+++
@@ -1,9 +0,0 @@
-const styleToolbar = {
-  backgroundColor: '#F7F9FC',
-  height: '64px',
-  paddingRight: '20px',
-};
-
-module.exports = {
-  styleToolbar,
-};
